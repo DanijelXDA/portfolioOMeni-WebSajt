dede4ebcd20f4ea068a5c5b368c7902ce213755b
danijelxda <danijelj2001.xda@gmail.com>
2018-09-14 09:13:09 +0200

    Update mailer script

--- sendMail.php
+++ sendMail.php
@@ -1,13 +1,13 @@
 <?php 
-	$name = $_POST['ime'];
+	$ime = $_POST['name'];
 	$email = $_POST['email'];
-	$message = $_POST['poruka'];
-	$formcontent="Име: $name \nE-адреса: $email\nПорука: $message\n";
+	$message = $_POST['message'];
+	$formcontent="Име: $ime \nE-адреса: $email\nПорука: $message\n";
 	$recipient = "danijelj2001.xda@gmail.com";
 	$subject = "Питање/Сугестија | Техничка школа";
-	$headers = 'From: no-reply@tehnickaskola.edu.rs' . "\r\n" .
-		'Reply-To: no-reply@tehnickaskola.edu.rs' . "\r\n" .
+	$headers = 'From: no-reply@portfolio.edu.rs' . "\r\n" .
+		'Reply-To: no-reply@portfolio.edu.rs' . "\r\n" .
 		'X-Mailer: PHP/' . phpversion();
 	mail($recipient, $subject, $formcontent, $headers) or die("Greska!");
-	header( 'Location: success.html' );
+	header( 'Location: index.html' );
 ?>
