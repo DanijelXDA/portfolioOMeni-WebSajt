cec73bd9dede6f99844066cf78690d5821d17ce6
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2021-04-04 19:47:10 +0200

    Add success page and fix whitespaces

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,7 +1,7 @@
 <?php 
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
     $subject = $_POST['subject'];
     $phone = $_POST['phone'];
     
@@ -11,4 +11,5 @@
     $formcontent="Име: $name \nE-адреса: $email\nПорука: $message\nТелефон: $phone\nВреме слања: $vreme\n\n";
     $file = "../kontakt-forme";
     file_put_contents($file, "\xEF\xBB\xBF".  $formcontent,  FILE_APPEND); 
+    header("Location: sent.html")
 ?>
