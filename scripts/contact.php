b8e23920166e47f1ee7a6dd59afd6561c8bad663
Danijel Jovanović <jovanovic.pr55.2020@uns.ac.rs>
2021-04-04 19:35:35 +0200

    Fix line formatting

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,15 +1,11 @@
 <?php 
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$message = $_POST['message'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
     $subject = $_POST['subject'];
     $phone = $_POST['phone'];
+
+    $formcontent="Име: $name \nE-адреса: $email\nПорука: $message\nТелефон: $phone\n";
     
-    date_default_timezone_get ('Europe/Belgrade');
-    $vreme = date("d.m.Y H:i", time());
-
-    $formcontent="Име: $name \nE-адреса: $email\nПорука: $message\nТелефон: $phone\nВреме слања: $vreme\n\n";
-    $file = "../kontakt-forme";
-    file_put_contents($file, "\xEF\xBB\xBF".  $formcontent,  FILE_APPEND); 
+    file_put_contents("kontakt-forme.txt", $formcontent);
 ?>
-
